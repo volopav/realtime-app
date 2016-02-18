b77c60a1134f326832ba983f71ade26948329953
Volodymy Pavlyuk <vpavlyuk@spredfast.com>
2016-02-18 13:59:41 +0200

    Added few webpack loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,35 @@ module.exports = {
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin("./css/[name].css")
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+        loader: 'file?name=./fonts/[name].[ext]'
+      },
+      {
+        test   : /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', ['css', 'resolve-url'])
+      },
+      {
+        test: /\.less/,
+        loader: ExtractTextPlugin.extract("style-loader", ["css", 'resolve-url', "less"])
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=./images/[hash].[ext]',
+          'image-webpack?bypassOnDebug'
+        ]
+      }
+    ]
   }
 };
